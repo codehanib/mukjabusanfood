94da1f56b5e0c7e760b44f7b96490fc95fbe58cd
HHJ <hhyejin122@gmail.com>
2026-09-09 17:34:30 +0900

    09/09 5PM (HHJ) 리뷰 수정 삭제 기능

--- src/main/java/com/springboot/MUKJA/dao/reviewDAO.java
+++ src/main/java/com/springboot/MUKJA/dao/reviewDAO.java
@@ -12,11 +12,13 @@ import com.springboot.MUKJA.dto.reviewimgDTO;
 public interface reviewDAO {
 	
 	// 식당별 리뷰 갯수
-	public int reviewCount(int r_no);
+	public int reviewCount(@Param("r_no") int r_no);
 	// 식당별 리뷰 조회 -식당페이지-
-	public List<reviewDTO> reviewPList(int r_no);
+	public List<reviewDTO> reviewPList(@Param("r_no") int r_no);
 	// 식당별 리뷰 조회 -리뷰페이지-
-	public List<reviewDTO> reviewList(int r_no);
+	public List<reviewDTO> reviewList(@Param("r_no") int r_no);
+	// 리뷰 1개 조회 (수정페이지용)
+	public reviewDTO reviewUP(@Param("rv_no") int rv_no);
 	
 	// 리뷰 작성
 	public int reviewInsert(reviewDTO rvdto);
@@ -26,12 +28,12 @@ public interface reviewDAO {
 	// 리뷰 수정 - 본인만
 	public int reviewUpdate(reviewDTO rvdto);
 	// 리뷰 이미지 삭제 (수정용)
-	public int reviewimgDelete(int rv_no);
+	public int reviewimgDelete(@Param("rv_no") int rv_no);
 	
 	// 리뷰 삭제
 	public int reviewDelete(@Param("rv_no") int rv_no);
 	
 	// 리뷰 이미지 조회
-	public List<reviewimgDTO> reviewImgView(int rv_no);
+	public List<reviewimgDTO> reviewImgView(@Param("rv_no") int rv_no);
 	
 }
