d6194b9a06978ce2517ddbc707729707b39d501a
HHJ <hhyejin122@gmail.com>
2026-09-21 17:22:57 +0900

    0921 5PM (HHJ) 리뷰 이미지 수정방식 변경

--- src/main/java/com/springboot/MUKJA/service/reviewService.java
+++ src/main/java/com/springboot/MUKJA/service/reviewService.java
@@ -33,17 +33,20 @@ public class reviewService {
 	// 리뷰 수정
 	@Transactional
 	public void reviewUpdate(reviewDTO rvdto) {
-		rvdao.reviewUpdate(rvdto);
-		
-		// 기존 이미지 삭제하고 업로드
-		rvdao.reviewimgDelete(rvdto.getRv_no());
-		
-		if(rvdto.getReviewImages()!=null) {
-			for(reviewimgDTO rvimg : rvdto.getReviewImages()) {
-				rvimg.setRv_no(rvdto.getRv_no());
-				rvdao.reviewImgInsert(rvimg);
-			}
-		}
+
+	    // 리뷰 내용 / 평점 수정
+	    rvdao.reviewUpdate(rvdto);
+	    // 새 이미지가 있을 때만 기존 이미지 삭제 후 새 이미지 등록
+	    if (rvdto.getReviewImages() != null
+	            && !rvdto.getReviewImages().isEmpty()) {
+	        // 기존 이미지 삭제
+	        rvdao.reviewimgDelete(rvdto.getRv_no());
+	        // 새 이미지 등록
+	        for (reviewimgDTO rvimg : rvdto.getReviewImages()) {
+	            rvimg.setRv_no(rvdto.getRv_no());
+	            rvdao.reviewImgInsert(rvimg);
+	        }
+	    }
 	}
 	
 	// 식당 페이지에서 리뷰 3개 출력
